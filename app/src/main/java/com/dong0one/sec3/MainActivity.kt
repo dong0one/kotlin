fb15cbb4e4d22533508a2d2476a96a63e8c2f290
dong0one <dong0one9659@naver.com>
2022-07-24 13:03:16 +0900

    sec3: Log

---
+++ app/src/main/java/com/dong0one/sec3/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.dong0one.sec3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonLog.setOnClickListener {
+            Log.d("TAG", "메사지");
+        }
+    }
+
+}
